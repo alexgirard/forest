bd0ad0412801c76755da7b447b2e80764037968d
heathermusson <hmusson@cogeco.ca>
2020-06-19 13:44:15 -0400

    Setup some initial staff stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'homepage#index'
+  get 'dashboard', to: 'dashboard#index'
+
+  resources :staffs
   
   devise_for :users, controllers: { registrations: 'registrations', sessions: 'sessions' }
 end
