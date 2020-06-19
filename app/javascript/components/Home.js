75385349af65457580f740a228d22c507e3244be
Alexandra Girard <alexgirard999@gmail.com>
2020-06-19 11:22:11 -0400

    Login

--- app/javascript/components/Home.js
+++ app/javascript/components/Home.js
@@ -1,12 +1,32 @@
 import React from 'react';
 
+
+const Login = ({ changePage }) => <p>Login</p>;
+const Signup = ({ changePage }) => <p>Signup</p>;
+const Edit = ({ changePage }) => <p>Edit</p>;
+
+
 class Home extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Homepage</h1>
-      </div>
-    );
+  constructor(props) {
+    super(props);
+    this.state = { page: 'login' };
+  };
+
+ changePage = newPage => this.setState({ page: newPage });
+
+ render() {
+   const { page } = this.state;
+
+    switch(page) {
+      case 'login':
+        return <Login changePage={this.changePage}/>
+      case 'signup':
+        return <Signup changePage={this.changePage}/>
+      case 'edit':
+        return <Edit changePage={this.changePage}/>
+      default:
+        return <p>defualt</p>
+    }
   }
 }
 
