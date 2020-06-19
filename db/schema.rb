e3b9724c35e441d929bfde6a34d4d73686ffe011
Heather <havmusson@uwaterloo.ca>
2020-06-19 12:48:32 -0400

    Add rooms table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_19_164049) do
+ActiveRecord::Schema.define(version: 2020_06_19_164649) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "rooms", force: :cascade do |t|
+    t.string "identifier", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "staffs", force: :cascade do |t|
     t.integer "badge", null: false
