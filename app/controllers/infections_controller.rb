24ef60ae31cb55412d2a8744adec1b0c5969cc5c
heathermusson <hmusson@cogeco.ca>
2020-06-19 19:50:08 -0400

    Create infection actions

---
+++ app/controllers/infections_controller.rb
@@ -0,0 +1,29 @@
+class InfectionsController < ApplicationController 
+  def create 
+    infection = Infection.new
+    infection.notes = params[:infection][:notes]
+    infection.start = params[:infection][:start]
+    infection.incubation = params[:infection][:incubation]
+    infection.steps = params[:infection][:steps]
+    patient = Patient.find(params[:infection][:patient_id])
+    infection.patient = patient
+    infection.save!
+  end
+
+  def show 
+    infection = Infection.find(params[:id])
+    render component: 'InfectionDetails', props: {infection: infection}
+  end
+
+  def update 
+    infection = Infection.find(params[:id])
+    infection.update_attributes(params.permit(:notes, :start, :end, :incubation, :steps))
+    redirect_to dashboard_path
+  end
+
+  def destroy 
+    infection = Infection.find(params[:id])
+    infection.destroy
+    redirect_to dashboard_path
+  end
+end
---
+++ app/javascript/components/InfectionDetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class InfectionDetails extends React.Component {
+  render() {
+    return (
+      <h1>Infection Details</h1>
+    );
+  }
+};
+
+export default InfectionDetails;
